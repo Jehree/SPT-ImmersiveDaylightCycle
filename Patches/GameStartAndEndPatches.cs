12fc3b2bc8896a6d3dd1f4da59c311fc30c1d057
Jehree <126540981+Jehree@users.noreply.github.com>
2024-07-29 21:07:32 -0400

    3.9.X update

--- Patches/GameStartAndEndPatches.cs
+++ Patches/GameStartAndEndPatches.cs
@@ -1,4 +1,4 @@
-﻿using Aki.Reflection.Patching;
+﻿using SPT.Reflection.Patching;
 using Comfort.Common;
 using EFT;
 using Jehree.ImmersiveDaylightCycle.FikaNetworking;
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Aki.Reflection.Utils;
+using SPT.Reflection.Utils;
 
 namespace Jehree.ImmersiveDaylightCycle.Patches {
 
